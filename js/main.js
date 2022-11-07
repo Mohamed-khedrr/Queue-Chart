d238a11a5e61d672853142c7bd2c051921c02870
Mohamed <khedr66664@gmail.com>
2022-11-07 23:21:09 +0200

    setting chart js library

---
+++ js/main.js
@@ -0,0 +1,91 @@
+"use strict"
+
+// VARIABLES 
+let period = 100;
+// the arrival time ratio
+let lamda = 4,
+    meu = 6,
+    capacity = 4;
+
+// CHART TIME LINE
+const labels = [];
+
+// CHAT VALUES
+let values = [];
+
+// EQUATION TO CALCULATE CUSTOMERS AT SPECIFIC TIME
+function getCustomers(lamda, meu, t, capacity) {
+
+    let servingTime = meu, arrivingTime = lamda, waiting = 0;
+    let arr = [];
+    for (let i = 0; i < arrivingTime; i++) {
+        arr.push(0);
+        // push the x Axis numbers
+        labels.push(i);
+    }
+
+    for (let i = arrivingTime; i <= period; i++) {
+        if ((i - arrivingTime) % servingTime == 0 && i > servingTime && waiting > 0) waiting--;
+        if (i % arrivingTime == 0 && waiting < capacity) waiting++;
+        arr[i] = waiting;
+
+        // push the x Axis numbers
+        labels.push(i);
+    }
+    return arr;
+}
+
+
+values = getCustomers(lamda, meu, period, capacity);
+
+
+
+// DATA OF THE CHART
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Customers over time',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: values,
+        fill: false,
+        stepped: true,
+        pointStyle: '',
+        pointRadius: 0,
+        pointBorderColor: 'rgb(0, 0, 0)'
+    }]
+};
+
+// CONFIG OF THE CHART
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        interaction: {
+            intersect: false,
+            axis: 'x'
+        },
+        plugins: {
+            title: {
+                display: true,
+                text: (ctx) => 'Step ' + ctx.chart.data.datasets[0].stepped + ' Interpolation',
+            },
+            legend: {
+                labels: {
+                    usePointStyle: false,
+                },
+            }
+        }
+    }
+};
+
+// =========================================
+
+
+//  intialize Chart
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
